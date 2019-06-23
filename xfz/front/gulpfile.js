da3551495ef3b210318d84ab0189b022c02ef20e
caoyingfeng <164972038@qq.com>
2019-06-23 16:51:28 +0800

    rotation chart finish

--- xfz/front/gulpfile.js
+++ xfz/front/gulpfile.js
@@ -7,6 +7,10 @@ var concat = require("gulp-concat");
 // var imagemin = require("gulp-imagemin");
 var bs = require('browser-sync').create();
 var sass = require('gulp-sass');
+// 此插件有一个log方法，可以打印当前js错误的信息
+var util = require("gulp-util");
+// js出错后，可以看到原js具体出错位置
+var sourcemaps = require("gulp-sourcemaps");
 
 var path = {
     'html': './templates/**/',
@@ -37,9 +41,10 @@ gulp.task('css',function () {
 // 定义处理js文件的任务
 gulp.task('js',function () {
     gulp.src(path.js + '*.js')
-        .pipe(uglify())
+        .pipe(sourcemaps.init())
+        .pipe(uglify().on("error",util.log))
         .pipe(rename({'suffix':'.min'}))
-        // .pipe(sourcemaps.write())
+        .pipe(sourcemaps.write())
         .pipe(gulp.dest(path.js_dist))
         .pipe(bs.stream())
 });
